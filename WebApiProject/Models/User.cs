e7fea4ceae24b89a7912b65e51ec45a1e81144cf
mta135 <mihaitamazlicaru1959@gmail.com>
2020-10-30 13:13:50 +0200

    add age fro User

--- WebApiProject/Models/User.cs
+++ WebApiProject/Models/User.cs
@@ -10,6 +10,8 @@ namespace WebApiProject.Models
         public int Id { get; set; }
         public string Name { get; set; }
 
+        public int Age { get; set; }
+
         public int CompanyId { get; set; }
         public Company Company { get; set; }  // компания пользователя
     }
