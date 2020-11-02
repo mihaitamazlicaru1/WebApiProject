2b11412e64f8f55c874c377db0107d6763116c1b
mta135 <mihaitamazlicaru1959@gmail.com>
2020-11-02 12:14:54 +0200

    commit 1

---
+++ WebApplication1/Models/EmployeesViweModels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class EmployeesViweModels
+    {
+        public int EmployeeID { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string EmailID { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+    }
+}
